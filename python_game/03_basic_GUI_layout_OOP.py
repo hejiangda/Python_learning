66a0b68f3083ca156e2c2a4d90f2552c3ca9023e
hejiangda <hejiangda@163.com>
2020-05-26 00:16:01 +0800

    basic gui layout OOP version class __init__ __name__ '__main__'

---
+++ python_game/03_basic_GUI_layout_OOP.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+class Game(tk.Frame):
+    def __init__(self,master):
+        super(Game,self).__init__(master)
+        self.lives=3
+        self.width=610
+        self.height=400
+        self.canvas=tk.Canvas(self,bg='#aaaaff',
+                              width=self.width,
+                              height=self.height)
+        self.canvas.pack()
+        self.pack()
+
+if __name__=='__main__':
+    root=tk.Tk()
+    root.title('Hello,Pong!')
+    game=Game(root)
+    game.mainloop()
