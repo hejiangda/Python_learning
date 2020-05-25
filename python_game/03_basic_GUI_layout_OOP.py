96194e3628e35c448701b7867eb58981d4615518
hejiangda <hejiangda@163.com>
2020-05-26 00:32:59 +0800

    create GameObject class and test it

--- python_game/03_basic_GUI_layout_OOP.py
+++ python_game/03_basic_GUI_layout_OOP.py
@@ -11,9 +11,29 @@ class Game(tk.Frame):
                               height=self.height)
         self.canvas.pack()
         self.pack()
+class GameObject(object):
+    def __init__(self,canvas,item):
+        self.canvas=canvas
+        self.item=item
+
+    def get_position(self):
+        return self.canvas.coords(self.item)
+
+    def move(self,x,y):
+        self.canvas.move(self.item,x,y)
+
+    def delete(self):
+        self.canvas.delete(self.item)
 
 if __name__=='__main__':
     root=tk.Tk()
     root.title('Hello,Pong!')
     game=Game(root)
+    # game.canvas.create_rectangle(250,300,330,320,fill='blue',tags='paddle')
+    # item=game.canvas.create_rectangle(10,10,100,80,fill='green')
+    # game_object=GameObject(game.canvas,item)
+    # print(game_object.get_position())
+    # game_object.move(20,-10)
+    # print(game_object.get_position())
+    # game_object.delete()
     game.mainloop()
