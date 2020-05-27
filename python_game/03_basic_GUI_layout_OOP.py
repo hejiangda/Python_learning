c73a8bbaf8fe529c9237d6a61544e41296ed148a
hejiangda <hejiangda@163.com>
2020-05-27 21:59:17 +0800

    collide bricks

--- python_game/03_basic_GUI_layout_OOP.py
+++ python_game/03_basic_GUI_layout_OOP.py
@@ -61,6 +61,35 @@ class Game(tk.Frame):
     def start_game(self):
         pass
 
+    def update(self):
+        coords=self.get_position()
+        width=self.canvas.winfo_width()
+        if coords[0]<=0 or coords[2]>=width:
+            self.direction[0]*=-1
+        if coords[1]<=0:
+            self.direction[1]*=-1
+        x=self.direction[0]*self.speed
+        y=self.direction[1]*self.speed
+        self.move(x,y)
+
+    def collide(self,game_objects):
+        coords=self.get_position()
+        x=(coords[0]+coords[2])/2
+        if len(game_objects)>1:
+            self.direction[1]*=-1
+        elif len(game_objects)==1:
+            game_object=game_objects[0]
+            coords=game_object.get_position()
+            if x>coords[2]:
+                self.direction[0]=1
+            elif x<coords[0]:
+                self.direction[0]=-1
+            else:
+                self.direction[1]*=-1
+
+        for game_object in game_objects:
+            if isinstance(game_object,Brick):
+                game_object.hit()
 
 class GameObject(object):
     def __init__(self,canvas,item):
