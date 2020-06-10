22b806fd014cf69c129859018445f4eae18d20c4
hejiangda <hejiangda@163.com>
2020-06-10 23:44:24 +0800

    first version

--- python_game/03_basic_GUI_layout_OOP.py
+++ python_game/03_basic_GUI_layout_OOP.py
@@ -25,9 +25,9 @@ class Game(tk.Frame):
         self.setup_game()
         self.canvas.focus_set()
         self.canvas.bind('<Left>',
-                         lambda _:self.paddle.move(-10))
+                         lambda _:self.paddle.move(-50))
         self.canvas.bind('<Right>',
-                         lambda _:self.paddle.move(10))
+                         lambda _:self.paddle.move(50))
     def setup_game(self):
         self.add_ball()
         self.update_lives_text()
@@ -59,18 +59,34 @@ class Game(tk.Frame):
             self.canvas.itemconfig(self.hud,text=text)
 
     def start_game(self):
-        pass
+        self.canvas.unbind('<space>')
+        self.canvas.delete(self.text)
+        self.paddle.ball=None
+        self.game_loop()
 
-    def update(self):
-        coords=self.get_position()
-        width=self.canvas.winfo_width()
-        if coords[0]<=0 or coords[2]>=width:
-            self.direction[0]*=-1
-        if coords[1]<=0:
-            self.direction[1]*=-1
-        x=self.direction[0]*self.speed
-        y=self.direction[1]*self.speed
-        self.move(x,y)
+    def game_loop(self):
+        self.check_collisions()
+        num_bricks=len(self.canvas.find_withtag('brick'))
+        if num_bricks == 0:
+            self.ball.speed=None
+            self.draw_text(300,200,'You win!')
+        elif self.ball.get_position()[3]>=self.height:
+            self.ball.speed=None
+            self.lives-=1
+            if self.lives<0:
+                self.draw_text(300,200,'Game Over!')
+            else:
+                self.after(1000,self.setup_game)
+        else:
+            self.ball.update()
+            self.after(50,self.game_loop)
+
+    def check_collisions(self):
+        ball_coords=self.ball.get_position()
+        items=self.canvas.find_overlapping(*ball_coords)
+        objects=[self.items[x] for x in items \
+                 if x in self.items]
+        self.ball.collide(objects)
 
     def collide(self,game_objects):
         coords=self.get_position()
@@ -114,6 +130,37 @@ class Ball(GameObject):
                                 x+self.radius,y+self.radius,
                                 fill='white')
         super(Ball,self).__init__(canvas,item)
+
+    def update(self):
+        coords=self.get_position()
+        width=self.canvas.winfo_width()
+        if coords[0]<=0 or coords[2]>=width:
+            self.direction[0]*=-1
+        if coords[1]<=0:
+            self.direction[1]*=-1
+        x=self.direction[0]*self.speed
+        y=self.direction[1]*self.speed
+        self.move(x,y)
+
+    def collide(self,game_objects):
+        coords=self.get_position()
+        x=(coords[0]+coords[2])*0.5
+        print('game_objects size:', len(game_objects))
+        if len(game_objects)>1:
+            self.direction[1]*=-1
+        elif len(game_objects)==1:
+            game_object=game_objects[0]
+            coords=game_object.get_position()
+            if x>coords[2]:
+                self.direction[0]=1
+            elif x<coords[0]:
+                self.direction[0]=-1
+            else:
+                self.direction[1]*=-1
+
+        for game_object in game_objects:
+            if isinstance(game_object,Brick):
+                game_object.hit()
 
 class Paddle(GameObject):
     def __init__(self,canvas,x,y):
